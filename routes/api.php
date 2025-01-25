cd756adaafaf6a6820e80508a86e013499206ed9
Szymon Bogdał <bogdal.symon@gmail.com>
2025-01-25 21:32:45 +0100

    add inform route

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->json([
+        'message' => ['Aviailable routes:','POST /modules','GET /modules/{id}/download']
+    ]);
+});
 Route::post('/modules', [App\Http\Controllers\ModuleController::class, 'createModule']);
 Route::get('/modules/{id}/download', [App\Http\Controllers\ModuleController::class, 'downloadModule']);
