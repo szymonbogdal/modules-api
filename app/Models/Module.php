25d0e939fff9121461f863e154e8e599bf50e4e5
Szymon Bogdał <bogdal.symon@gmail.com>
2025-01-25 20:48:36 +0100

    Module model typo fix

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Modules extends Model
+class Module extends Model
 {
     protected $fillable = ['width', 'height', 'color', 'link'];
 }
